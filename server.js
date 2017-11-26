7e88178407f44a743c76d96aa3e634b0656e3680
Marwen Trabelsi <marwen.projob@gmail.com>
2017-11-26 17:06:02 +0100

    added universal rendering

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = new Express()
 
 // define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, 'dist')))
+app.use('/customer',Express.static(path.join(__dirname, 'dist')))
+
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // start the server
 const port = process.env.PORT || 8000
